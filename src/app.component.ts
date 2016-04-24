673e42809497825eaa4ba9af5612db527a874e5f
Juho Friman <juho.t.friman@gmail.com>
2016-04-24 13:30:28 +0300

    Super simple angular2/webpack build env

---
+++ src/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'regex-app',
+    template: '<h1>Regular expression tester thingy</h1>'
+})
+export class AppComponent { }
+
+console.log("Totan");
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 
 module.exports = {
-  entry: './src/app.ts',
+  entry: {
+    'polyfills': ['es6-shim/es6-shim.js', 'angular2/bundles/angular2-polyfills'],
+    'vendor': './src/vendor.ts',
+    'main': './src/main.ts'
+  },
   output: {
-    filename: 'dist/bundle.js'
+    path: 'dist',
+    filename: 'js/[name].js',
+    chunkFilename: '[id].chunk.js'
   },
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.js']
@@ -13,7 +21,37 @@ module.exports = {
       { test: /\.ts$/, loader: 'ts-loader' }
     ]
   },
-  plugins: [new HtmlWebpackPlugin({
-    filename: "dist/index.html"
+  plugins: [
+    new CommonsChunkPlugin({
+      name: ['vendor', 'polyfills']
+    }),
+    new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    filename: "index.html",
+    inject: 'body',
+    chunksSortMode: packageSort(['polyfills', 'vendor', 'main'])
   })]
+}
+
+function packageSort(packages) {
+  // packages = ['polyfills', 'vendor', 'app']
+  var len = packages.length - 1;
+  var first = packages[0];
+  var last = packages[len];
+  return function sort(a, b) {
+    // polyfills always first
+    if (a.names[0] === first) {
+      return -1;
+    }
+    // main always last
+    if (a.names[0] === last) {
+      return 1;
+    }
+    // vendor before app
+    if (a.names[0] !== first && b.names[0] === last) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
 }
