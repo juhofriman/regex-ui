79fe6ad9047fceda0f9a01629ec2d004792167e1
Juho Friman <juho.t.friman@gmail.com>
2016-04-26 20:10:17 +0300

    Ok, this is how webpack loaders work.
    Wonder, if this has possibily of be deployable to github.io...

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+declare var require: {
+    <T>(path: string): T;
+    (paths: string[], callback: (...modules: any[]) => void): void;
+    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+};
+
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './components/app.component';
 
